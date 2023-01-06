370edb27e015224184d68c8a82efed5730320280
Yusaku <hitoyasablue@gmail.com>
2023-01-06 09:29:29 +0900

    add args to initialize method

--- arp.rb
+++ arp.rb
@@ -1,7 +1,7 @@
 class Arp
   attr_reader :hardware_type, :protocol_type, :hardware_size, :protocol_size, :opcode, :sender_mac_addr,
               :sender_ip_addr, :target_mac_addr, :target_ip_addr
-  def initialize
+  def initialize(hardware_type, protocol_type, hardware_size, protocol_size, opcode, sender_mac_addr, sender_ip_addr, target_mac_addr, target_ip_addr)
     @hardware_type = hardware_type
     @protocol_type = protocol_type
     @hardware_size = hardware_size
@@ -13,3 +13,6 @@ class Arp
     @target_ip_addr = target_ip_addr
   end
 end
+
+arp = Arp.new("a", "a", 1, 2, "a", "a", "a", "a", "a")
+p arp
--- ethernet.rb
+++ ethernet.rb
@@ -2,9 +2,12 @@ class Ethernet
   # TODO: checksumを含める
   attr_reader :type, :dst_mac_addr, :src_mac_addr
 
-  def initialze
+  def initialize(type, dst_mac_addr, src_mac_addr)
     @type = type
-    @dst_mac = dst_mac
-    @src_mac = src_mac
+    @dst_mac_addr = dst_mac_addr
+    @src_mac_addr = src_mac_addr
   end
 end
+
+ether = Ethernet.new("ipv4", "1111", "2222")
+p ether
