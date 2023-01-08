45ed759314a3d98bdabb6317e391343c83a69941
Yusaku <hitoyasablue@gmail.com>
2023-01-08 21:02:30 +0900

    fix target mac addr in arp initialize

--- arp.rb
+++ arp.rb
@@ -1,8 +1,7 @@
 class Arp
   attr_reader :hardware_type, :protocol_type, :hardware_size, :protocol_size, :opcode, :sender_mac_addr,
               :sender_ip_addr, :target_mac_addr, :target_ip_addr
-  # TODO:initializeは一部でいいかも？
-  def initialize(sender_mac_addr, sender_ip_addr, target_mac_addr, target_ip_addr)
+  def initialize(sender_mac_addr, sender_ip_addr, target_ip_addr)
     @hardware_type = 0x0001
     @protocol_type = 0x0800
     @hardware_size = 0x06
@@ -10,7 +9,10 @@ class Arp
     @opcode = 0x0001
     @sender_mac_addr = sender_mac_addr
     @sender_ip_addr = sender_ip_addr
-    @target_mac_addr = 0x0000000000000000
+    @target_mac_addr = 0x000000000000
     @target_ip_addr = target_ip_addr
   end
 end
+
+a = Arp.new(1, 1, 1)
+p a
